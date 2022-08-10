69d13f065b20bf4014e2d220102dee1c4b6b8e14
ZenithGD <darimarcos456@hotmail.com>
2022-08-10 14:01:40 +0200

    remove repo services

--- components/navigation/Navbar.js
+++ components/navigation/Navbar.js
@@ -16,24 +16,29 @@ function Navbar() {
 
     return (
         <div className={`z-50 fixed transition-all duration-200 top-0 w-full h-14 flex justify-center gap-4 ${scroll ? 'bg-gray-850 filter drop-shadow-md' : ''}`}>
+            <Link href="/">
+                <button className='w-28'>
+                    <p className="text-xl text-cyan-100 font-bold fromRight">Inicio</p>
+                </button>
+            </Link>
             <Link href="bio">
                 <button className='w-28'>
                     <p className="text-xl text-cyan-100 font-bold fromRight">Bio</p>
                 </button>
             </Link>
-            <Link href="hobbies">
+            <Link href="blog">
                 <button className='w-28'>
-                    <p className="text-xl text-cyan-100 font-bold fromCenter">Hobbies</p>
+                    <p className="text-xl text-cyan-100 font-bold fromCenter">Blog</p>
                 </button>
             </Link>
             <Link href="projects">
                 <button className='w-28'>
-                    <p className="text-xl text-cyan-100 font-bold fromCenter">Projects</p>
+                    <p className="text-xl text-cyan-100 font-bold fromCenter">Proyectos</p>
                 </button>
             </Link>
             <Link href="contact">
                 <button className='w-28'>
-                    <p className="text-xl text-cyan-100 font-bold fromLeft">Contact</p>
+                    <p className="text-xl text-cyan-100 font-bold fromLeft">Contacto</p>
                 </button>
             </Link>
         </div>
--- pages/projects.js
+++ pages/projects.js
@@ -1,18 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import useSWR from "swr";
+import React from 'react';
 
 import { motion } from "framer-motion"
 
 import { wvContainer, wvItem } from "../animation/indexVariants"
-import RepoCard from '../components/ui/RepoCard';
-import repoService from '../services/repoService';
 
 function Projects() {
-
-    const { data: repos, error: err } = useSWR(
-        process.env.NEXT_PUBLIC_API_URL + "/getrepos",
-        repoService.getRepos
-    );
 
     return (
         <div className='min-h-screen'>
@@ -28,20 +20,16 @@ function Projects() {
                         variants={wvItem}
                         className='mb-10 text-4xl font-bold text-cyan-200'
                     >
-                        Mis proyectos
+                        Mis proyectos destacados
                     </motion.h1>
                     <motion.p
                         className="font-light my-5 text-xl text-white"
                         variants={wvItem}
                     >
-                        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+                        aaa
                     </motion.p>
                 </motion.div>
-                {(repos && repos.length > 0) ? (
-                    repos.map((r, key) => <RepoCard key={key} repo={r} />)
-                ) : (
-                    <p>...</p>
-                )}
+                
             </div>
         </div>
     );
