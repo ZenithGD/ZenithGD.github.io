cc757ab8efa41f0e5646fed825d497d28c6f1ef0
ZenithGD <darimarcos456@hotmail.com>
2022-10-25 20:29:47 +0200

    redeploy

--- components/ui/timeline/Timeline.js
+++ components/ui/timeline/Timeline.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import TimelineItem from './TimelineItem'
 
+import './Timeline.module.css'
+
 function Timeline() {
   return (
-    <div>
-      <TimelineItem />
+    <div className='relative w-full flex flex-col timeline-container'>
+      <TimelineItem title="Primer paso"/>
+      <TimelineItem title="Segundo paso"/>
     </div>
   )
 }
--- components/ui/timeline/TimelineItem.js
+++ components/ui/timeline/TimelineItem.js
@@ -1,14 +1,21 @@
 import Link from 'next/link'
 import React from 'react'
 
-function TimelineItem({title, children}) {
+import './Timeline.module.css'
+
+function TimelineItem({title, text, link}) {
   return (
-    <>
+    <div className='bg-gray-700 drop-shadow-md p-4 timeline-element'>
       <h3 className='text-cyan-300'>{title}</h3>
       <div>
-        {children}
+        {text}
       </div>
-    </>
+      {link && (
+        <Link href={link}>
+          Ir al enlace {'>'}
+        </Link>
+      )}
+    </div>
   )
 }
 
