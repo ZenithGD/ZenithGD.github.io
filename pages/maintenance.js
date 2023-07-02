5e4d1adcc7474e1b1f9e17faed6103e4e51d62bb
ZenithGD <darimarcos456@hotmail.com>
2023-07-02 02:50:59 +0200

    switch to another random cat generation page

--- pages/maintenance.js
+++ pages/maintenance.js
@@ -20,7 +20,7 @@ function Maintenance() {
           <div className="className='mx-auto text-center flex flex-col justify-between gap-6">
             <p className='font-bold text-7xl text-cyan-100'>¡Alto ahí!</p>
             <p className='font-bold text-5xl text-cyan-100'>Página en mantenimiento.</p>
-            <p className='text-cyan-500 text-xl'>¿Por qué no consultar <a href='https://random.cat' className='underline'>fotos de gatitos</a> durante la espera?</p>
+            <p className='text-cyan-500 text-xl'>¿Por qué no consultar <a href={`https://cataas.com/cat/says/${encodeURIComponent('¡Miau! (¡Estamos trabajando!)')}`} className='underline'>fotos de gatitos</a> durante la espera?</p>
         
           </div>
           <div className='flex-1 self-center'>
